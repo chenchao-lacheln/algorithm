1ee8406a1717bd9705304ae9374d1db7bec5a96e
chenchao <chenchao@qq.com>
2022-07-30 11:25:57 +0800

    数组模拟队列

--- DataStructures/src/com/lacheln/quene/ArrayQueueDemo.java
+++ DataStructures/src/com/lacheln/quene/ArrayQueueDemo.java
@@ -1,5 +1,7 @@
 package com.lacheln.quene;
 
+import java.util.Scanner;
+
 /**
  * ArrayQueneDemo
  *
@@ -9,7 +11,53 @@ package com.lacheln.quene;
  */
 public class ArrayQueueDemo {
     public static void main(String[] args) {
-
+        //创建一个队列
+        ArrayQueue queue = new ArrayQueue(3);
+        char key = ' ';//接收用户输入
+        Scanner scanner = new Scanner(System.in);
+        boolean loop = true;
+        //输入一个菜单
+        while (loop) {
+            System.out.println("s（show）：显示队列");
+            System.out.println("e（exit）：退出程序");
+            System.out.println("a（add）：添加数据到队列");
+            System.out.println("g（get）：从队列取出数据");
+            System.out.println("h（head）：查看队列头数据");
+            key = scanner.next().charAt(0); //接收一个字符
+            switch (key) {
+                case 's':
+                    queue.showQueue();
+                    break;
+                case 'a':
+                    System.out.println("输入一个数到队列");
+                    int value = scanner.nextInt();
+                    queue.addQueue(value);
+                    break;
+                case 'g':
+                    try {
+                        int res = queue.getQueue();
+                        System.out.printf("取出的数据是%d\n", res);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'h':
+                    try {
+                        int res = queue.headQueue();
+                        System.out.printf("队列头的数据是%d\n",res);
+                    }catch (Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'e':
+                    scanner.close();
+                    loop = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+        System.out.println("程序退出~~");
     }
 }
 
@@ -21,7 +69,7 @@ class ArrayQueue {
     private int[] arr; //用户存放数据，模拟队列
 
     //创建队列构造器
-    public ArrayQueue (int arrMaxSize){
+    public ArrayQueue(int arrMaxSize) {
         maxSize = arrMaxSize;
         arr = new int[maxSize];
         front = -1; //指向队列头部，分析front是指向队列头的前一个位置
@@ -29,19 +77,19 @@ class ArrayQueue {
     }
 
     //判断队列是否满
-    public boolean isFull(){
+    public boolean isFull() {
         return rear == maxSize - 1;
     }
 
     //判断队列是否为空
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return rear == front;
     }
 
     //添加数据到队列
-    public void addQueue(int n){
+    public void addQueue(int n) {
         //判断队列是否满
-        if (isFull()){
+        if (isFull()) {
             System.out.println("队列满，不能添加数据~");
             return;
         }
@@ -50,9 +98,9 @@ class ArrayQueue {
     }
 
     //获取队列数据，出队列
-    public int getQueue(){
+    public int getQueue() {
         //判断队列是否为空
-        if (isEmpty()){
+        if (isEmpty()) {
             throw new RuntimeException("队列为空，不能取数据");
         }
         front++; //front 后移
@@ -60,19 +108,19 @@ class ArrayQueue {
     }
 
     //显示队列所有数据
-    public void showQueue(){
-        if (isEmpty()){
+    public void showQueue() {
+        if (isEmpty()) {
             System.out.println("队列为空，没有数据~");
             return;
         }
-        for (int i = 0;i < arr.length;i++){
-            System.out.printf("arr[%d] = %d\n",i,arr[i]);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("arr[%d] = %d\n", i, arr[i]);
         }
     }
 
     //显示队列的头数据，不是取数据
-    public int headQueue(){
-        if (isEmpty()){
+    public int headQueue() {
+        if (isEmpty()) {
             throw new RuntimeException("队列为空，没有数据~");
         }
         return arr[front + 1];
