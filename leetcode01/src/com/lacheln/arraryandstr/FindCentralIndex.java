a2a1b33c8788939ca88a48f871b54dee0b12cc2d
chenchao <742352449@qq.com>
2022-08-12 19:19:07 +0800

    add FindCentralIndex

---
+++ leetcode01/src/com/lacheln/arraryandstr/FindCentralIndex.java
@@ -0,0 +1,23 @@
+package com.lacheln.arraryandstr;
+
+/**
+ * FindCentralIndex
+ * 寻找中心索引
+ * @author chenchao
+ * @date 2022/8/12 2:21 PM
+ * @since 1.0.0
+ */
+public class FindCentralIndex {
+    public static void main(String[] args) {
+
+    }
+}
+
+/**
+ * 给你一个整数数组nums ，请计算数组的中心下标 。
+ * 数组 中心下标 是数组的一个下标，其左侧所有元素相加的和等于右侧所有元素相加的和。
+ * 如果中心下标位于数组最左端，那么左侧数之和视为 0 ，因为在下标的左侧不存在元素。这一点对于中心下标位于数组最右端同样适用。
+ * 如果数组有多个中心下标，应该返回 最靠近左边 的那一个。如果数组不存在中心下标，返回 -1 。
+ * 分析:
+ * 1.
+ */
