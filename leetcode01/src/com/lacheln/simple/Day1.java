0499263729e83a7759e3a7ea87840310a33545c6
chenchao <742352449@qq.com>
2023-06-20 14:44:31 +0800

    通过hashmap解决两数之和

--- leetcode01/src/com/lacheln/simple/Day1.java
+++ leetcode01/src/com/lacheln/simple/Day1.java
@@ -1,6 +1,8 @@
 package com.lacheln.simple;
 
 
+import java.util.HashMap;
+
 /**
  * Day1
  *
@@ -10,13 +12,21 @@ package com.lacheln.simple;
  */
 public class Day1 {
     public static void main(String[] args) {
-        //2.测试回文数
-        int num2 = 12353321;
-        if (Solution.isPalindrome(num2)) {
-            System.out.println(num2 + "是回文数");
-        } else {
-            System.out.println(num2 + "不是回文数");
+        //1.测试两数之和
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        int[] res = Solution.twoSum2(nums,target);
+        for (int i = 0;i < res.length;i++){
+            System.out.print(i + " ");
         }
+
+        //2.测试回文数
+//        int num2 = 12353321;
+//        if (Solution.isPalindrome(num2)) {
+//            System.out.println(num2 + "是回文数");
+//        } else {
+//            System.out.println(num2 + "不是回文数");
+//        }
     }
 }
 
@@ -25,7 +35,8 @@ class Solution {
      * 1.求2数之和
      * 定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
      */
-    public int[] twoSum(int[] nums, int target) {
+    //1.循环遍历
+    public static int[] twoSum1(int[] nums, int target) {
         //增加对null和target为空的判断，并抛出异常
         if (nums == null || nums.length == 0 || target == 0) {
             throw new IllegalArgumentException("Invalid input!!");
@@ -39,6 +50,24 @@ class Solution {
             }
         }
         return new int[0];
+    }
+
+    //2.用哈希表解决
+    public static int[] twoSum2(int[] nums, int target) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        //遍历数组nums
+        for (int i = 0; i < nums.length; i++) {
+            //目标数值
+            int complement = target - nums[i];
+            //判断hashMap中是否存在目标值，如果存在，就返回一个新数组，数组中是2个元素的下标
+            if (hashMap.containsKey(complement)){
+                return new int[] {hashMap.get(complement),i};
+            }
+            //如果不存在目标值，则将当前元素num[i]，和下标添加到hashMap中
+            hashMap.put(nums[i],i);
+        }
+        //没有找到，返回空数组
+        return new int[] {};
     }
 
     /**
