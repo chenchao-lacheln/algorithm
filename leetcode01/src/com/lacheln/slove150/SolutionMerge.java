e1481ac2024d75efa731d409ed772434332d776b
chenchao <742352449@qq.com>
2023-11-07 16:42:37 +0800

    add:移除元素

--- leetcode01/src/com/lacheln/slove150/SolutionMerge.java
+++ leetcode01/src/com/lacheln/slove150/SolutionMerge.java
@@ -44,7 +44,31 @@ public class SolutionMerge {
      * <p>
      * 进阶：你可以设计实现一个时间复杂度为 O(m + n) 的算法解决此问题吗？
      */
+    public static void main(String[] args) {
+        int[] num1 = {1,2,3,0,0,0};
+        int m = 3;
+        int n = 3;
+        int[] num2 = {2,5,6};
+        new SolutionMerge().merge(num1,m,num2,n);
+    }
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-
+        int p1 = 0, p2 = 0;
+        int[] sorted = new int[m + n];
+        int cur;
+        while (p1 < m || p2 < n) {
+            if (p1 == m) {
+                cur = nums2[p2++];
+            } else if (p2 == n) {
+                cur = nums1[p1++];
+            } else if (nums1[p1] < nums2[p2]) {
+                cur = nums1[p1++];
+            } else {
+                cur = nums2[p2++];
+            }
+            sorted[p1 + p2 - 1] = cur;
+        }
+        for (int i = 0; i != m + n; ++i) {
+            nums1[i] = sorted[i];
+        }
     }
 }
