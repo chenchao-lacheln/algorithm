8c3e4842db70df1d0aa1bd140703d6a32b4114e1
chenchao <742352449@qq.com>
2024-02-22 11:48:16 +0800

    update:双指针解决->验证字符串是否是回文字符串

--- leetcode01/src/com/lacheln/InterviewClassic150Q/IsPalindrome.java
+++ leetcode01/src/com/lacheln/InterviewClassic150Q/IsPalindrome.java
@@ -9,10 +9,10 @@ package com.lacheln.InterviewClassic150Q;
  */
 public class IsPalindrome {
     public static void main(String[] args) {
-        System.out.println(chenIsPalindrome("A man, a plan, a canal: Panama"));
-        System.out.println(chenIsPalindrome("race a car"));
-        System.out.println(chenIsPalindrome(" "));
-        System.out.println(chenIsPalindrome("1"));
+        System.out.println(chenIsPalindrome1("A man, a plan, a canal: Panama"));
+        System.out.println(chenIsPalindrome1("race a car"));
+        System.out.println(chenIsPalindrome1(" "));
+        System.out.println(chenIsPalindrome1("1"));
     }
 
     /**
@@ -20,7 +20,7 @@ public class IsPalindrome {
      *
      * @return 验证回文字符串
      */
-    public static boolean chenIsPalindrome(String s) {
+    public static boolean chenIsPalindrome1(String s) {
         /*
         如果在将所有大写字符转换为小写字符、并移除所有非字母数字字符之后，短语正着读和反着读都一样。则可以认为该短语是一个 回文串 。
         给你一个字符串 s，如果它是 回文串 ，返回 true ；否则，返回 false 。
@@ -35,5 +35,15 @@ public class IsPalindrome {
         }
         StringBuffer stringBuffer_rev = new StringBuffer(stringBuilder).reverse();
         return stringBuilder.toString().equals(stringBuffer_rev.toString());
+    }
+
+    /**
+     * 双指针
+     *
+     * @param s
+     * @return
+     */
+    public static boolean chenIsPalindrome2(String s) {
+
     }
 }
