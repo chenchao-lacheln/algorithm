94872e13174fc97ce69187b8d2c75068b71ea9a5
chenchao <742352449@qq.com>
2024-03-04 19:38:42 +0800

    update:描述

--- leetcode01/src/com/lacheln/InterviewClassic150Q/Merge.java
+++ leetcode01/src/com/lacheln/InterviewClassic150Q/Merge.java
@@ -30,6 +30,13 @@ public class Merge {
     其中前 m 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
 
      */
+
+    /**
+     * @param num1
+     * @param m
+     * @param num2
+     * @param n    合并后，直接排序
+     */
     public static void chenMerge(int[] num1, int m, int[] num2, int n) {
         for (int i = 0; i < n; i++) {
             num1[m + i] = num2[i];
