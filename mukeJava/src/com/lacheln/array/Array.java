832c43988d534cbf3784a7ed6ebbea4db1ff382c
chenchao <742352449@qq.com>
2024-03-05 12:30:34 +0800

    fix:格式错误

--- mukeJava/src/com/lacheln/array/Array.java
+++ mukeJava/src/com/lacheln/array/Array.java
@@ -65,7 +65,7 @@ public class Array {
     @Override
     public String toString() {
         StringBuilder res = new StringBuilder();
-        res.append(String.format("Array：size = %d , capacity = %d\n"), size, data.length);
+        res.append(String.format("Array：size = %d , capacity = %d\n", size, data.length));
         res.append('[');
         for (int i = 0; i < size; i++) {
             res.append(data[i]);
