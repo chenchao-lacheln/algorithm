eebb7aab2a2e3f515ceb313d1b682fd9fb9bad37
chenchao <742352449@qq.com>
2024-03-08 19:34:29 +0800

    add:栈

---
+++ mukeJava/src/com/lacheln/stack/ArrayStack.java
@@ -0,0 +1,36 @@
+package com.lacheln.stack;
+
+/**
+ * ArrayStack
+ *
+ * @author chenchao
+ * @date 2024/3/8
+ * @since 1.0.0
+ * 基于动态数组实现栈
+ */
+public class ArrayStack<E> implements Stack<E> {
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void push(E e) {
+
+    }
+
+    @Override
+    public E pop() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+}
---
+++ mukeJava/src/com/lacheln/stack/Stack.java
@@ -0,0 +1,21 @@
+package com.lacheln.stack;
+
+/**
+ * Stack
+ *
+ * @author chenchao
+ * @date 2024/3/8
+ * @since 1.0.0
+ */
+public interface Stack<E> {
+    //返回int型
+    int getSize();
+    //返回boolean型
+    boolean isEmpty();
+    //向栈中添加元素
+    void push(E e);
+    //从栈中取出元素
+    E pop();
+    //栈顶元素
+    E peek();
+}
