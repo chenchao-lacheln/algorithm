2849c9b1d7e40d6dc52da0abaea8e6d373316726
chenchao <742352449@qq.com>
2024-03-11 16:59:31 +0800

    add:栈的应用：解决有效括号，算法问题

---
+++ mukeJava/src/com/lacheln/stack/validparentheses/Solution.java
@@ -0,0 +1,39 @@
+package com.lacheln.stack.validparentheses;
+
+import com.lacheln.stack.ArrayStack;
+import com.lacheln.stack.Stack;
+
+/**
+ * Sloution
+ *
+ * @author chenchao
+ * @date 2024/3/11
+ * @since 1.0.0
+ * 用栈：解决有效括号算法问题
+ */
+public class Solution {
+    public boolean isvalid(String s) {
+        Stack<Character> stack = new ArrayStack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char topChar = stack.pop();
+                if (c == ')' && topChar != '(') {
+                    return false;
+                }
+                if (c == '[' && topChar != ']') {
+                    return false;
+                }
+                if (c == '{' && topChar != '}') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty(); //注意：最终只有当stack栈为空时，才算是匹配成功
+    }
+}
