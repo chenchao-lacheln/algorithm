beb7939c0cbc1552dd8f9149360e2eae42b56736
chenchao <742352449@qq.com>
2024-03-11 17:14:16 +0800

    add:用java自带栈库，解决有效括号算法问题

--- mukeJava/src/com/lacheln/stack/validparentheses/Solution.java
+++ mukeJava/src/com/lacheln/stack/validparentheses/Solution.java
@@ -1,7 +1,8 @@
 package com.lacheln.stack.validparentheses;
 
-import com.lacheln.stack.ArrayStack;
-import com.lacheln.stack.Stack;
+//import com.lacheln.stack.ArrayStack;
+//import com.lacheln.stack.Stack;
+import java.util.Stack;
 
 /**
  * Sloution
@@ -9,11 +10,11 @@ import com.lacheln.stack.Stack;
  * @author chenchao
  * @date 2024/3/11
  * @since 1.0.0
- * 用栈：解决有效括号算法问题
+ * 用java自带的栈库：解决有效括号算法问题
  */
 public class Solution {
     public boolean isvalid(String s) {
-        Stack<Character> stack = new ArrayStack<>();
+        Stack<Character> stack = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if (c == '(' || c == '[' || c == '{') {
@@ -35,5 +36,10 @@ public class Solution {
             }
         }
         return stack.isEmpty(); //注意：最终只有当stack栈为空时，才算是匹配成功
+    }
+
+    public static void main(String[] args) {
+        System.out.println((new Solution()).isvalid("()[]{}"));
+        System.out.println((new Solution()).isvalid("([)]"));
     }
 }
