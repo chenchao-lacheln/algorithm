e5428212fa0fe56cbfbf29812d2fbfb424f136a7
chenchao <742352449@qq.com>
2024-03-20 12:22:33 +0800

    add:增加链表的遍历，查询，修改

---
+++ mukeJava/src/com/lacheln/linkedlist/Main.java
@@ -0,0 +1,20 @@
+package com.lacheln.linkedlist;
+
+/**
+ * Main
+ *
+ * @author chenchao
+ * @date 2024/3/20
+ * @since 1.0.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        for (int i = 0; i < 5; i++) {
+            linkedList.addFirst(i);
+            System.out.println(linkedList);
+        }
+        linkedList.add(2, 888);
+        System.out.println(linkedList);
+    }
+}
